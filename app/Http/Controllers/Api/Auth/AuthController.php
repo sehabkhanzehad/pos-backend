7dfc004bd0796b7c689d945656135b5c03de5e1a
Sk Zehad <sehabkhanzehad@yahoo.com>
2026-01-13 21:28:54 +0600

    Implement AuthController for user authentication with sign-up, sign-in, and sign-out functionality

---
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rules;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signUp(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        User::create($validated);
+
+        return response()->json(['message' => 'Sign up successful.'], 201);
+    }
+
+    public function signIn(Request $request): JsonResponse
+    {
+        $validated = $request->validate([
+            'email' => ['required', 'string', 'email'],
+            'password' => ['required', 'string'],
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+
+        $credentials = $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) return response()->json(['message' => 'The provided credentials are incorrect.'], 401);
+
+        /** @var User $user */
+        $user = Auth::user();
+
+        $tokenExpiration = $validated['remember'] ? now()->addMonths(6) : now()->addHours(12);
+
+        $token = $user->createToken('Mobile Token', ['*'], $tokenExpiration);
+
+        return response()->json([
+            'accessToken' => $token->plainTextToken,
+            'tokenType' => 'Bearer',
+            'expiresAt' => $tokenExpiration->toDateTimeString(),
+        ]);
+    }
+
+    public function signOut(Request $request): JsonResponse
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json(['message' => 'Sign out successful.']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::prefix('auth')->group(function () {
+    Route::middleware('guest')->group(function () {
+        Route::post('/sign-up', [AuthController::class, 'signUp']);
+        Route::post('/sign-in', [AuthController::class, 'signIn']);
+    });
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/sign-out', [AuthController::class, 'signOut']);
+    });
+});
